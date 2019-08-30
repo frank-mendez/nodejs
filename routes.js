6091314186bad40421eb92c30baf2c0e1bbe99ac
Frank Mendez <frankmendezwebdev@gmail.com>
2019-08-30 19:57:21 +0800

    feat(package): Added npm

--- routes.js
+++ routes.js
@@ -7,13 +7,21 @@ const requestHandler = (req, res) => {
 
     if (url === '/') {
         res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<head><title>Hello World</title></head>');
+        res.write('<body><h1>Greetings!</h1><form action="/create-user" method="POST"><input type="text" name="user"><button type="submit">Create</button></form></body>');
         res.write('</html>');
         return res.end();
     }
-    
-    if (url === '/message' && method === 'POST') {
+
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><ul><li>Frank</li><li>Demby</li><li>Geof</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/create-user' && method === 'POST') {
         const body = [];
         req.on('data', chunk => {
             console.log(chunk);
@@ -21,14 +29,11 @@ const requestHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            const user = parsedBody.split('=')[1];
+            console.log(user);
         });
     }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>First Node JS Page</title></head>');
@@ -37,5 +42,12 @@ const requestHandler = (req, res) => {
     res.end();
 
 };
+
+//module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded Text'
+// };
 
 module.exports = requestHandler;
