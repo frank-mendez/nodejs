70b420a826093f12cba45f7b592ef3c8bbe4aba8
Frank Mendez <frankmendezwebdev@gmail.com>
2019-09-03 10:47:26 +0800

    feat(express): Added express js and helper functions

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded());
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
