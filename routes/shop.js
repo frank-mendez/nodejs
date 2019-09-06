c1f01ada768b280cd50fab79151b58201700547b
Frank Mendez <frankmendezwebdev@gmail.com>
2019-09-06 11:30:18 +0800

    feat(data): Sharing Data Across Requests and Users

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const adminData = require('./admin');
 
 const rootDir = require('../util/path');
 
 router.get('/',(req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
