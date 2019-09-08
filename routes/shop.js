9ed47f4bb6d97e97badb9e65b1305e46f588715c
Frank Mendez <frankmendezwebdev@gmail.com>
2019-09-08 12:37:00 +0800

    feat(handlebars): Converting our Project to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const router = express.Router();
 const path = require('path');
-const adminData = require('./admin');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
-router.get('/',(req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Frank Shop', path: '/'});
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
