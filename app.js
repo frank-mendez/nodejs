41687ebfd521cea37e63f11ef75f41c0826f4ed8
Frank Mendez <frankmendezwebdev@gmail.com>
2019-09-09 20:34:16 +0800

    feat(404): Finishing the Controllers

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// );
-// app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -27,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: false });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
