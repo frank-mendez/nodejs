e6b608867c402e5723d0c737be27a39087d90006
Frank Mendez <frankmendezwebdev@gmail.com>
2019-09-09 20:43:48 +0800

    feat(model): Adding a Product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
- const products = [];
- 
- exports.getAddProduct = (req, res, next) => {
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { 
       pageTitle: 'Add Product', 
       path: '/admin/add-product', 
@@ -10,18 +10,20 @@
     });
   };
 
-  exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
-  };
+};
 
-  exports.getProducts = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
     });
-  };
+};
