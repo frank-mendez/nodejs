eff874af3869ba03fcaf4c2ad703d1a81c6bcccd
frankmendez <frankmendezwebdev@gmail.com>
2021-01-11 22:34:56 +0800

    Inserting Data Into the Database

--- models/product.js
+++ models/product.js
@@ -11,7 +11,14 @@ module.exports = class Product {
 		this.price = price
 	}
 
-	save() {}
+	save() {
+		return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [
+			this.title,
+			this.price,
+			this.imageUrl,
+			this.description,
+		])
+	}
 
 	static deleteById(id) {}
 
