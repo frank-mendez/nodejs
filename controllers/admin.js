aca594c6d683f398fd046630d74e1686cadaa216
frankmendez <frankmendezwebdev@gmail.com>
2021-01-13 01:47:05 +0800

    Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
 	})
 		.then((result) => {
 			console.log('Created A product')
+			res.redirect('/admin/products')
 		})
 		.catch((err) => console.log(err))
 }
@@ -81,6 +82,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId
-	Product.deleteById(prodId)
-	res.redirect('/admin/products')
+	Product.findByPk(prodId)
+		.then((product) => {
+			return product.destroy()
+		})
+		.then((result) => {
+			res.redirect('/admin/products')
+		})
+		.catch((err) => console.log(err))
 }
